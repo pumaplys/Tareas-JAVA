f8aec9cd38433e8a63676f720bd47034fa802be8
Eduardo Puma <pumaplys@gmail.com>
2025-10-20 19:41:36 +0200

    Ejercicios Avanzados

---
+++ src/tema2/t02/ej08.java
@@ -0,0 +1,12 @@
+package tema2.t02;
+// Muestra los números primos entre 1 y 100.
+public class ej08 {
+    public static void main(String[] args) {
+
+        for (int i =1; i <= 100; i++){
+            if (i % 2 == 0){
+                System.out.println(i);
+            }
+        }
+    }
+}
