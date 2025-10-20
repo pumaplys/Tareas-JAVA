7025021e63fb5eae4d6a86bc5876825ede825e09
Eduardo Puma <pumaplys@gmail.com>
2025-10-20 19:28:27 +0200

    Ejercicios Avanzados

--- src/tema2/t02/ej06.java
+++ src/tema2/t02/ej06.java
@@ -6,6 +6,16 @@ public class ej06 {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
 
+        System.out.println("Introduce las ventas que has hecho: ");
+        int ventas = sc.nextInt();
 
+        double suma = 0;
+
+        for (int i = 1; i <= ventas; i++){
+            System.out.println("Introduce el monto de la venta " + i + ":");
+            double monto = sc.nextDouble();
+            suma += monto;
+        }
+        System.out.println("El total de todas las ventas es: " + suma);
     }
 }
