2406603edf9b95cce6e71cb1ae108f3192f2e745
Eduardo Puma <pumaplys@gmail.com>
2025-10-29 17:11:47 +0100

    ejercicios recursivos

---
+++ src/tema2/t04/ej01.java
@@ -0,0 +1,11 @@
+package tema2.t04;
+
+import java.util.Scanner;
+
+// Implementa un método recursivo que determine si un número es primo.
+public class ej01 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+    }
+}
